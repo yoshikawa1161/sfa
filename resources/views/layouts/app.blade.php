2cc57ea0ee24bcb14e43b41bf9f406b88bc5d0bf
yoshikawa1161 <t_ic3063@yahoo.co.jp>
2023-09-14 21:56:32 +0900

    app.blde.phpを編集

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,6 @@
 
         <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.js"></script>
         <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
 
         <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.8/index.global.min.js'></script>
         <meta name="csrf-token" content="{{ csrf_token() }}">
