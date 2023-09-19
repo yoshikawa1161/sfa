7c135d74cb13d0f95c7a6304d4bb1d184747d7f1
yoshikawa1161 <t_ic3063@yahoo.co.jp>
2023-09-19 21:23:24 +0900

    本番環境に適応させるため、mapのマーカーを修正

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
-use Illuminate\Support\facades\DB;
+use \Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class Report extends Model
